d7e7ffe53424d4b7eb07d6d35af9bdf8a6acfac0
dsrodolfo <rodolfod91@hotmail.com>
2020-01-25 15:31:39 -0300

    Project version 1.0.0

---
+++ WebAPI_ASP_Demo/Controllers/StudentsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI_ASP_Demo.Models;
+
+namespace WebAPI_ASP_Demo.Controllers
+{
+    public class StudentsController : ApiController
+    {
+        private static List<Student> students = new List<Student>();
+
+        [HttpGet]
+        public List<Student> Get() => students;
+
+        [HttpPost]
+        public void Post(string name, string country, int age)
+        {
+            if (!String.IsNullOrWhiteSpace(name)) { students.Add(new Student(name, country, age)); }
+        }
+        
+        [HttpDelete]
+        public void Delete(string name)
+        {
+            students.RemoveAt(students.IndexOf(students.First(x => x.Name.Equals(name))));
+        }
+    }
+}
---
+++ WebAPI_ASP_Demo/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI_ASP_Demo.Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public int Age { get; set; }
+
+        public Student(string name, string country, int age)
+        {
+            this.Name = name;
+            this.Country = country;
+            this.Age = age;
+        }
+    }
+}
